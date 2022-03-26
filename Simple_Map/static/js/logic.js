f1b64f9c8a35c34de2c154738d33c89df540ecc6
Lucas <lucasbratland@gmail.com>
2022-03-25 21:02:00 -0500

    updating module polyline work

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,8 +1,21 @@
 //  Add console.log to check if code is working
 console.log("working");
 
-// Create the map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with a center and zoom level original coords 40.7, -94.5 zoom 4
+let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+
+// Coords to be used in the line
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+// Create a polyline suing the line coords and make the line red
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
 
 // Add marker ot the map for LA
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -66,16 +79,16 @@ let cityData = cities
 // });
 
 // Create circles with radius based on population
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circle(city.location, {
-        radius: city.population/15
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circle(city.location, {
+//         radius: city.population/15
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
 
-// We create the tile later that will be the backgroud of the map
+// // We create the tile later that will be the backgroud of the map
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
@@ -86,7 +99,17 @@ cityData.forEach(function(city) {
 // }).addTo(map);
 
 // We create the tile later that will be the backgroud of the map
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     // id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY
+// }).addTo(map);
+
+// We create the tile later that will be the backgroud of the map (satellite)
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox/streets-v11',
@@ -94,3 +117,4 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tile
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(map);
+
